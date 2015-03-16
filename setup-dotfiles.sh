78a23a8f197149af6b4afe20107f8dd5c433a8db
Artem Popov <artem.popov@outlook.com>
2015-03-16 18:51:41 +0500

    remove old file

--- setup-dotfiles.sh
+++
@@ -1,7 +0,0 @@
-
-
-git config --global branch.autosetuprebase always
-git config --global branch.master.rebase true
-git config --global color.ui true
-
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
